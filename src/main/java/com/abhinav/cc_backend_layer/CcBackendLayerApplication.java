0501c1cc118235205c0c0d39ba474349e381168a
Abhinav Sharma <theabhinavsharma@hotmail.com>
2024-12-04 02:50:18 +0530

    Initial commit

---
+++ src/main/java/com/abhinav/cc_backend_layer/CcBackendLayerApplication.java
@@ -0,0 +1,13 @@
+package com.abhinav.cc_backend_layer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcBackendLayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcBackendLayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abhinav/cc_backend_layer/CcBackendLayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhinav.cc_backend_layer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcBackendLayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
