96d3dc3b351bdfd71469fd84e1d60daa72852368
Abhinav Sharma <theabhinavsharma@hotmail.com>
2025-06-01 11:28:50 +0530

    Updated pool size and min idle

--- src/main/java/com/abhinav/cc_backend_layer/repository/EmployeeAttendanceRepository.java
+++ src/main/java/com/abhinav/cc_backend_layer/repository/EmployeeAttendanceRepository.java
@@ -1,9 +1,11 @@
 package com.abhinav.cc_backend_layer.repository;
 
+import java.sql.Date;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.abhinav.cc_backend_layer.model.EmployeeAttendance;
 
-public interface EmployeeAttendanceRepository extends JpaRepository<EmployeeAttendance, String> {
+public interface EmployeeAttendanceRepository extends JpaRepository<EmployeeAttendance, Date> {
 
 }
--- src/main/java/com/abhinav/cc_backend_layer/repository/UserSessionRepository.java
+++ src/main/java/com/abhinav/cc_backend_layer/repository/UserSessionRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.abhinav.cc_backend_layer.model.UserSession;
 
-public interface UserSessionRepository extends JpaRepository<UserSession, String>{
+public interface UserSessionRepository extends JpaRepository<UserSession, Integer>{
 	
 	UserSession findTopByUsernameOrderBySessionidDesc(String username);
 	UserSession findFirstByOrderBySessionidDesc();
