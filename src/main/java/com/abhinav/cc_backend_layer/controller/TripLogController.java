599114a6abac9415477ebe40955213706fa0d13a
Abhinav Sharma <theabhinavsharma@hotmail.com>
2026-07-23 12:33:26 +0530

    Updated deletion

--- src/main/java/com/abhinav/cc_backend_layer/controller/TripLogController.java
+++ src/main/java/com/abhinav/cc_backend_layer/controller/TripLogController.java
@@ -6,7 +6,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,5 +37,14 @@ public class TripLogController {
 	public ResponseEntity<List<PendingTripLog>> getPendingTrips() {
 	    return ResponseEntity.ok(pendingTripLogRepository.findAll());
 	}
+	
+	@DeleteMapping("/{id}")
+    public ResponseEntity<?> deletePendingTrip(@PathVariable Long id) {
+        if (!pendingTripLogRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        pendingTripLogRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
 }
 
